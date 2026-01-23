d717c1e6d173c51f32ef9ecf1949d7a2b44c076a
nor0x <nor0x@users.noreply.github.com>
2026-01-23 13:02:30 +0100

    net10.0

--- scripts/build-macos.sh
+++ scripts/build-macos.sh
@@ -11,14 +11,14 @@ echo "Building Dots for macOS arm64"
 dotnet msbuild -t:BundleApp -property:Configuration=Release -p:UseAppHost=true -p:RuntimeIdentifier=osx-arm64
 
 echo "Prepare App Bundle for arm64"
-rm bin/Release/net8.0-macos/osx-arm64/publish/Dots.app/Contents/MacOS/*.pkg
-cp Assets/AppIcon.icns bin/Release/net8.0-macos/osx-arm64/publish/Dots.app/Contents/Resources/
-cp -Rf bin/Release/net8.0-macos/osx-arm64/Dots.app/Contents/MacOS bin/Release/net8.0-macos/osx-arm64/publish/Dots.app/Contents
-cp -Rf bin/Release/net8.0-macos/osx-arm64/Dots.app/Contents/MonoBundle bin/Release/net8.0-macos/osx-arm64/publish/Dots.app/Contents
-cp bin/Release/net8.0-macos/osx-arm64/Dots.app/Contents/PkgInfo bin/Release/net8.0-macos/osx-arm64/publish/Dots.app/Contents/
+rm bin/Release/net10.0-macos/osx-arm64/publish/Dots.app/Contents/MacOS/*.pkg
+cp Assets/AppIcon.icns bin/Release/net10.0-macos/osx-arm64/publish/Dots.app/Contents/Resources/
+cp -Rf bin/Release/net10.0-macos/osx-arm64/Dots.app/Contents/MacOS bin/Release/net10.0-macos/osx-arm64/publish/Dots.app/Contents
+cp -Rf bin/Release/net10.0-macos/osx-arm64/Dots.app/Contents/MonoBundle bin/Release/net10.0-macos/osx-arm64/publish/Dots.app/Contents
+cp bin/Release/net10.0-macos/osx-arm64/Dots.app/Contents/PkgInfo bin/Release/net10.0-macos/osx-arm64/publish/Dots.app/Contents/
 
 echo "codesign Dots for macOS arm64"
-APP_NAME="/Users/runner/work/Dots/Dots/src/bin/Release/net8.0-macos/osx-arm64/publish/Dots.app"
+APP_NAME="/Users/runner/work/Dots/Dots/src/bin/Release/net10.0-macos/osx-arm64/publish/Dots.app"
 ENTITLEMENTS="/Users/runner/work/Dots/Dots/scripts/Dots.entitlements"
 
 echo "[INFO]______________[INFO] Signing app files"
@@ -42,7 +42,7 @@ echo "[INFO]______________[INFO] Signing app file"
 codesign --force --timestamp --options=runtime --entitlements "$ENTITLEMENTS" --sign "$SIGNING_IDENTITY" "$APP_NAME"
 
 echo "dittoing Dots for macOS arm64"
-cd /Users/runner/work/Dots/Dots/src/bin/Release/net8.0-macos/osx-arm64/publish
+cd /Users/runner/work/Dots/Dots/src/bin/Release/net10.0-macos/osx-arm64/publish
 macosarm64file=$(echo Dots-$version-macos-arm64.zip)
 ditto -c -k --sequesterRsrc --keepParent Dots.app $macosarm64file
 xcrun notarytool submit $macosarm64file --apple-id $APPLE_ID --team-id $TEAM_ID --password $APP_SPECIFIC_PWD --verbose --wait
@@ -52,14 +52,14 @@ echo "Building Dots for macOS x64"
 dotnet msbuild -t:BundleApp -property:Configuration=Release -p:UseAppHost=true -p:RuntimeIdentifier=osx-x64
 
 echo "Prepare App Bundle for x64"
-rm bin/Release/net8.0-macos/osx-x64/publish/Dots.app/Contents/MacOS/*.pkg
-cp Assets/AppIcon.icns bin/Release/net8.0-macos/osx-x64/publish/Dots.app/Contents/Resources/
-cp -Rf bin/Release/net8.0-macos/osx-x64/Dots.app/Contents/MacOS bin/Release/net8.0-macos/osx-x64/publish/Dots.app/Contents
-cp -Rf bin/Release/net8.0-macos/osx-x64/Dots.app/Contents/MonoBundle bin/Release/net8.0-macos/osx-x64/publish/Dots.app/Contents
-cp bin/Release/net8.0-macos/osx-x64/Dots.app/Contents/PkgInfo bin/Release/net8.0-macos/osx-x64/publish/Dots.app/Contents/
+rm bin/Release/net10.0-macos/osx-x64/publish/Dots.app/Contents/MacOS/*.pkg
+cp Assets/AppIcon.icns bin/Release/net10.0-macos/osx-x64/publish/Dots.app/Contents/Resources/
+cp -Rf bin/Release/net10.0-macos/osx-x64/Dots.app/Contents/MacOS bin/Release/net10.0-macos/osx-x64/publish/Dots.app/Contents
+cp -Rf bin/Release/net10.0-macos/osx-x64/Dots.app/Contents/MonoBundle bin/Release/net10.0-macos/osx-x64/publish/Dots.app/Contents
+cp bin/Release/net10.0-macos/osx-x64/Dots.app/Contents/PkgInfo bin/Release/net10.0-macos/osx-x64/publish/Dots.app/Contents/
 
 echo "codesign Dots for macOS x64"
-APP_NAME="/Users/runner/work/Dots/Dots/src/bin/Release/net8.0-macos/osx-x64/publish/Dots.app"
+APP_NAME="/Users/runner/work/Dots/Dots/src/bin/Release/net10.0-macos/osx-x64/publish/Dots.app"
 ENTITLEMENTS="/Users/runner/work/Dots/Dots/scripts/Dots.entitlements"
 
 echo "[INFO]______________[INFO] Signing app files"
@@ -83,7 +83,7 @@ echo "[INFO]______________[INFO] Signing app file"
 codesign --force --timestamp --options=runtime --entitlements "$ENTITLEMENTS" --sign "$SIGNING_IDENTITY" "$APP_NAME"
 
 echo "dittoing Dots for macOS x64"
-cd /Users/runner/work/Dots/Dots/src/bin/Release/net8.0-macos/osx-x64/publish
+cd /Users/runner/work/Dots/Dots/src/bin/Release/net10.0-macos/osx-x64/publish
 macosx64file=$(echo Dots-$version-macos-x64.zip)
 ditto -c -k --sequesterRsrc --keepParent Dots.app $macosx64file
 xcrun notarytool submit $macosx64file --apple-id $APPLE_ID --team-id $TEAM_ID --password $APP_SPECIFIC_PWD --verbose --wait
